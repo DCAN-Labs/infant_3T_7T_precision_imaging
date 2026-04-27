d05fa01ef07672e187d569277868f6effa85a1e3
moser297 <90271498+moser297@users.noreply.github.com>
2026-04-26 20:36:48 -0500

    Add files via upload

---
+++ connectivity/plot_scan_effectors_connections_revision.R
@@ -0,0 +1,53 @@
+setwd("C:/Users/moser297/Box/ION/ION_Data_analysis/Baby3T-7Tpaper/submission_NatureComms/revision") #directory with data tables
+library(tidyverse)
+library(NHANES)
+#library(remotes) for pattern
+#library(ggpattern) for pattern
+# library(lme4) # Package that provides multilevel modeling with lmer()
+# library(lmerTest) # Used to estimate degrees of freedom for LMER
+# library(emmeans) #for post hoc test with lmer
+
+data_overview <- read.csv( "results_scan_vs_effector_right.csv", header=TRUE, sep=",") #%>%
+  #filter(condition!='7T_highres')
+
+
+sum.connections <- data_overview %>%                               # Summary by group using dplyr
+  group_by(ID, magnet, network) %>% 
+  summarize(mean = mean(value),
+            std = sd(value))
+
+data_summary <- function(x) {
+  m <- mean(x)
+  ymin <- m-sd(x)
+  ymax <- m+sd(x)
+  return(c(y=m,ymin=ymin,ymax=ymax))
+}
+
+data_overview %>% 
+  ggplot(aes(
+    x=magnet, 
+    y=value, 
+    color=network, 
+    fill=network
+  )) +
+  geom_bar(stat = "summary", position = "dodge")+
+  geom_jitter(size=3, shape = 21, stroke = 1.5, fill="white", show.legend = FALSE, position=position_dodge(0.8))+
+  scale_color_manual(values = c("SCAN" = "#000000", "effector" = "#828282"))+
+  scale_fill_manual(values = c("SCAN" = "#000000", "effector" = "#828282"))+
+  ylab("connectivity") +
+  theme_bw() +
+  theme(
+    strip.text.y = element_text(size = 18),
+    text = element_text(size = 18),
+    legend.title = element_blank(),
+    axis.text.x=element_text(angle = 90, size=16, color="black", vjust=0.5),
+    axis.text.y=element_text(size=18, color="black"),
+    axis.title.y = element_text(size = 18),
+    axis.title.x = element_blank(),
+    #panel.border = element_blank(),
+    axis.line = element_line(colour = "black")
+    )+
+    #facet_grid(cols = vars(sub), rows = vars(space))
+    facet_grid(cols = vars(ID))
+#p + stat_summary(fun.data=data_summary, color="blue")
+
